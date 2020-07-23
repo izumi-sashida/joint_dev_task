a1f70a40d737ef7c6fc30065cfa50651a5ed22a7
izumi <zzznozu@gmail.com>
2020-07-23 23:03:23 +0900

    Q14.15.16回答

--- task.rb
+++ task.rb
@@ -144,6 +144,8 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
+  puts data1.include?(:age)? "OK" : "NG"
+  puts data2.include?(:age)? "OK" : "NG" 
 
 end
 
@@ -156,6 +158,9 @@ def q16
   ]
 
   # 以下に回答を記載
+  users.each do |user|
+    puts "私の名前は#{user[:name]}です。年齢は#{user[:age]}歳です。"
+  end
 
 end
 
